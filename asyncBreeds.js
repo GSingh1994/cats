55b4f403e7e555382d35d4979df0e9eb6fc85088
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-24 21:56:34 -0500

    Moved tests to /test folder

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -15,9 +15,5 @@ const breedDetailsFromFile = function (breed, callback) {
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
-const finalResult = (result) => {
-  console.log('finalResult: ', result);
-};
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', finalResult);
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+
+module.exports = breedDetailsFromFile;
